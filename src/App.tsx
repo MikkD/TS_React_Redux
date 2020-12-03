51800312e1ad462f1864bc69c9792b5e8894aca5
Nick Alexeev <ndosyukov@gmail.com>
2020-12-03 11:31:30 -0800

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,62 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import NavBar from './components/NavBar';
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
 
+export interface ITodoItem {
+  title: string,
+  id: number,
+  isCompleted: boolean
+}
+
 
 const App: React.FC = () => {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState<Array<ITodoItem>>([]);
+
+  useEffect(() => {
+    const storageTodos = JSON.parse(localStorage.getItem('todos') || '[]');
+    setTodos(storageTodos)
+
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+
+  }, [todos]);
+
 
   const addTodo = (todo: string) => {
+    const newTodo: ITodoItem = {
+      title: todo,
+      id: Date.now(),
+      isCompleted: false
+    };
+    setTodos(prevState => [newTodo, ...prevState])
+  }
+
+  const deleteTodo = (id: number) => setTodos(todos.filter(el => el.id !== id))
+
+  const toggleTodo = (id: number) => {
+    const upd_todos = todos.map(el => {
+      if (el.id === id) {
+        return {
+          ...el,
+          isCompleted: !el.isCompleted
+        }
+      }
+      return el
+    })
+    setTodos(upd_todos);
 
   }
+
+
   return (
     <div className="App">
       <NavBar />
       <div className="container">
         <TodoForm addTodo={addTodo} />
-        <TodoList />
+        <TodoList deleteTodo={deleteTodo} toggleTodo={toggleTodo} todos={todos} />
 
       </div>
 
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,23 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { ITodoItem } from '../App';
 
-const TodoList: React.FC = () => {
-    return (
-        <React.Fragment>
-            <ul className="collection">
-                <li className="collection-item">
-                    <div className="valign-wrapper" style={{ justifyContent: "space-between" }}>
-                        <label>
-                            <input type="checkbox" />
-                            <span>Red</span>
-                        </label>
-                        <span>
-                            <i className="small material-icons right ">delete_forever</i>
-                        </span>
-                    </div>
-                </li>
-            </ul>
-        </React.Fragment>
-    )
+interface TodoItems {
+    todos: ITodoItem[],
+    deleteTodo(id: number): void
+    toggleTodo(id: number): void
+}
+
+
+const TodoList: React.FC<TodoItems> = ({ todos, deleteTodo, toggleTodo }) => {
+
+    return todos.length > 0 ?
+        (
+            <React.Fragment>
+                <ul className="collection">
+                    {todos.map(todo => {
+                        return (
+                            <li key={todo.id} className="collection-item">
+                                <div className="valign-wrapper" style={{ justifyContent: "space-between" }}>
+                                    <label>
+                                        <input type="checkbox" checked={todo.isCompleted} onChange={() => toggleTodo(todo.id)} />
+                                        <span className={todo.isCompleted ? "completed" : "not_completed"}>{todo.title}</span>
+                                    </label>
+                                    <span
+                                        onClick={() => deleteTodo(todo.id)}
+                                        style={{ cursor: "pointer" }}>
+                                        <i className="small material-icons right ">delete_forever</i>
+                                    </span>
+                                </div>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </React.Fragment>
+        ) : <h5>Todo list is empty</h5>
 }
 
 export default TodoList;
